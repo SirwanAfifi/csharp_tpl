b318e5bc2102715024218bce613d4cd35e7e310f
Sirwan <sir1afifi@gmail.com>
2020-07-12 22:01:10 +0430

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace csharp_tpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class PriceEventArgs : EventArgs
+    {
+        public PriceEventArgs(string name, decimal price)
+        {
+            TimeStampUtc = DateTime.UtcNow;
+            Price = price;
+            Name = string.Intern(name);
+        }
+        public DateTime TimeStampUtc { get; }
+        public decimal Price { get; }
+        public string Name { get; }
+    }
+}
